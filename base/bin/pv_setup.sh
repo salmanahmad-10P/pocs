7ba9708e85e59aeba7040286ad55eb990603e0de
jbride <jbride2001@yahoo.com>
2020-07-29 09:10:18 -0600

    latest

--- base/bin/pv_setup.sh
+++ base/bin/pv_setup.sh
@@ -35,7 +35,7 @@ function create() {
     chmod -R 777 /u0$MAJOR_DISK/
 }
 
-function clean() {
+function cleanAll() {
     oc delete pv --all
     echo "$MOUNT_TARGET_DIR_ROOT               192.168.122.0/24(rw,sync,no_subtree_check,crossmnt,fsid=0)" > /etc/exports
     exportfs -ra
@@ -50,13 +50,31 @@ function clean() {
     done
 }
 
+function refresh() {
+    for ((MINOR_DISK=10;MINOR_DISK<=$MAX_PV_ID;MINOR_DISK++)); do 
+      # Create corresponding PV
+      lowercase="${MAJOR_DISK,,}"
+      name=pvu0$lowercase$MINOR_DISK
+      path=$DIR_NAME
+      status=$(oc get pv $name -o template --template {{.status.phase}})
+      echo -en "\nrefresh: $name $status\n"
+      if [ "Released" = $status ]; then
+        echo -en "\nAbout to refresh: $name\n"
+        oc delete pv $name
+        rm -rf /u0$MAJOR_DISK/$DIR_NAME/*
+        cat $SCRIPT_DIR/pv.yaml | sed "s/{name}/$name/g" | sed "s/{path}/$path/g" | oc create -f -
+      fi
+    done
+
+}
+
 for MAJOR_DISK in A B C; do
     case "$1" in
-        create|clean)
+        create|refresh|clean)
             $1
             ;;
         *)
-        echo 1>&2 $"Usage: $0 {create|clean}"
+        echo 1>&2 $"Usage: $0 {create|refresh|clean}"
         exit 1
     esac
 
