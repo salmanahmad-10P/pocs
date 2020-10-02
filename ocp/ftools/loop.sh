551be85d056f1f4e48baf206e473445e02989657
jbride <jbride2001@yahoo.com>
2020-10-01 18:18:30 -0600

    latest

--- ocp/ftools/loop.sh
+++ ocp/ftools/loop.sh
@@ -8,7 +8,7 @@ elif [ "$1" -ge 1 ]; then
     sleepSec=$1
 fi
 
-# Have the container loop indefinately so as to allow a user to manually rsh in an execute commands
+# Have the container loop indefinately so as to allow a user to manually rsh in and execute commands
 while /bin/true; do
     echo "Sleeping for the following seconds:" $sleepSec
     sleep $sleepSec
